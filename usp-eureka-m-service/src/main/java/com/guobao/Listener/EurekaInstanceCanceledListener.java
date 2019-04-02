d23338a41fad31e821747683d55eb987029fec12
wangzhil <sinosoft>
2019-04-02 22:11:32 +0800

    试试

--- usp-eureka-m-service/src/main/java/com/guobao/Listener/EurekaInstanceCanceledListener.java
+++ usp-eureka-m-service/src/main/java/com/guobao/Listener/EurekaInstanceCanceledListener.java
@@ -85,6 +85,7 @@ public class EurekaInstanceCanceledListener implements ApplicationListener {
 			log.info("[EurekaInstanceCanceledListener.onApplicationEvent] [服务 Aualiable444444444。。]");
 			log.info("[EurekaInstanceCanceledListener.onApplicationEvent] [服务 Aualiable444444444。。]");
 			log.info("[EurekaInstanceCanceledListener.onApplicationEvent] [服务 Aualiable444444444。。]");
+			log.info("[EurekaInstanceCanceledListener.onApplicationEvent] [服务 Aualiable442341243444。。]");
 			
 		}
 
